f3b5537fb7823957f4bc2b695b611c1474d8d06a
Tomás <tomas.bolano@protonmail.com>
2022-11-14 14:27:07 +0100

    Use single loop in AsyncModbusClient.read_registers method

--- modbus_client/client/async_modbus_client.py
+++ modbus_client/client/async_modbus_client.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import List, Sequence
+from typing import Any, Callable, Dict, List, Sequence
 
 from modbus_client.client.address_range import merge_address_ranges
 from modbus_client.client.registers import IRegister, RegisterType
@@ -50,59 +50,38 @@ class AsyncModbusClient:
         pass
 
     async def read_registers(
-            self, unit: int, registers: Sequence[IRegister],
-            allow_holes: bool = True) -> ModbusReadSession:
+        self,
+        slave: int,
+        registers: Sequence[IRegister],
+        allow_holes: bool = True,
+        max_read_size: int = 100,
+    ) -> ModbusReadSession:
 
-        coils_registers = \
-            [x for x in registers if
-             x.reg_type == RegisterType.Coil]
-        discrete_inputs_registers = \
-            [x for x in registers if
-             x.reg_type == RegisterType.DiscreteInputs]
-        input_registers = \
-            [x for x in registers if
-             x.reg_type == RegisterType.InputRegister]
-        holding_registers = \
-            [x for x in registers if
-             x.reg_type == RegisterType.HoldingRegister]
-
-        coils_buckets = merge_address_ranges(
-            coils_registers, allow_holes=False, max_read_size=1)
-        discrete_inputs_buckets = merge_address_ranges(
-            discrete_inputs_registers, allow_holes=False, max_read_size=1)
-        input_registers_buckets = merge_address_ranges(
-            input_registers, allow_holes=allow_holes, max_read_size=100)
-        holding_registers_buckets = merge_address_ranges(
-            holding_registers, allow_holes=allow_holes, max_read_size=100)
+        read_functions: Dict[RegisterType, Callable[..., Any]] = {
+            RegisterType.Coil: self.read_coils,
+            RegisterType.DiscreteInputs: self.read_discrete_inputs,
+            RegisterType.InputRegister: self.read_input_registers,
+            RegisterType.HoldingRegister: self.read_holding_registers,
+        }
 
         ses = ModbusReadSession()
-        for rng in coils_buckets:
-            coil_data = await self.read_coils(
-                unit=unit, address=rng.address, count=rng.count)
-            for i, val1 in enumerate(coil_data):
-                ses.registers_dict[
-                    (RegisterType.Coil, rng.address + i)] = val1
-
-        for rng in discrete_inputs_buckets:
-            discrete_inputs_data = await self.read_discrete_inputs(
-                unit=unit, address=rng.address, count=rng.count)
-            for i, val2 in enumerate(discrete_inputs_data):
-                ses.registers_dict[
-                    (RegisterType.DiscreteInputs, rng.address + i)] = val2
-
-        for rng in input_registers_buckets:
-            input_registers_data = await self.read_input_registers(
-                unit=unit, address=rng.address, count=rng.count)
-            for i, val3 in enumerate(input_registers_data):
-                ses.registers_dict[
-                    (RegisterType.InputRegister, rng.address + i)] = val3
-
-        for rng in holding_registers_buckets:
-            holding_registers_data = await self.read_holding_registers(
-                unit=unit, address=rng.address, count=rng.count)
-            for i, val4 in enumerate(holding_registers_data):
-                ses.registers_dict[
-                    (RegisterType.HoldingRegister, rng.address + i)] = val4
+        for reg_type in RegisterType:
+            discrete_reg = (
+                reg_type == RegisterType.Coil
+                or reg_type == RegisterType.DiscreteInputs
+            )
+            r = [x for x in registers if x.reg_type == reg_type]
+            buckets = merge_address_ranges(
+                r,
+                allow_holes=False if discrete_reg else allow_holes,
+                max_read_size=1 if discrete_reg else max_read_size,
+            )
+            for rng in buckets:
+                res = await read_functions[reg_type](
+                    slave=slave, address=rng.address, count=rng.count
+                )
+                for i, val1 in enumerate(res):
+                    ses.registers_dict[(reg_type, rng.address + i)] = val1
 
         return ses
 
